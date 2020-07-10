0af909609edea182bf9b88e457d04e7c8385f7c6
ShaoJieCui <2633742912@qq.com>
2020-07-10 13:38:37 +0800

    style: update css

--- frontend/src/component/SortPageCard.js
+++ frontend/src/component/SortPageCard.js
@@ -1,27 +1,46 @@
-import { Card,Collapse } from 'antd';
+import { Card,Collapse,Menu, Dropdown, Button } from 'antd';
 import React from "react";
 import "../css/HomeCard.css"
 import "../css/sortPage.css"
 const { Panel } = Collapse;
 const { Meta } = Card;
 
+const menu = (
+    <Menu>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                大麦网 ¥ 666
+            </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                小麦网 ¥ 777
+            </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                小米网 ¥ 888
+            </a>
+        </Menu.Item>
+    </Menu>
+);
+
 export class SortPageCard extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
     render(){
         return(
-            <div>
-                <div>
+            <div id="sortCard">
                     <img className='image' alt="example" src={this.props.info.imgurl} />
+                 <div>
                     <p>{this.props.info.title}</p>
                     <p>{'¥'+this.props.info.price}</p>
-                </div>
-                <div>
-                    <Collapse accordion>
-                        <Panel header="选择时间">
-                            <p>2020.07.07-07.12</p>
-                            <p>2020.07.08</p>
-                        </Panel>
-                    </Collapse>
-                </div>
+                    <Dropdown overlay={menu} placement="bottomCenter" arrow>
+                        <Button>购票</Button>
+                    </Dropdown>
+                 </div>
             </div>
         )
     }
