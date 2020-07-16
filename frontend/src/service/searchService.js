136b8395500e8a2a806e8caebcd798228e577dc2
mark-yyc <61784497+mark-yyc@users.noreply.github.com>
2020-07-16 15:53:08 +0800

    feat: addOrder backend

--- frontend/src/service/searchService.js
+++ frontend/src/service/searchService.js
@@ -2,6 +2,6 @@ import {postRequest,fetchPost1} from "../utils/ajax";
 
 export const search = (value,callback) => {
     const data={search:value};
-    const url = 'http://localhost:8080/Activity/search';
-    postRequest(url, data, callback);
+    const apiUrl = '/Activity/search';
+    postRequest(apiUrl, data, callback);
 };
