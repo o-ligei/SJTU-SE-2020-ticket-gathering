19b9ff641d21e17a3daaf87ed4173e1ae16d37b6
feaaaaaa <409834725@qq.com>
2020-07-16 12:46:10 +0800

    feat:fuzzy search

--- backend/src/main/java/com/oligei/ticket_gathering/daoimpl/ActivityDaoImpl.java
+++ backend/src/main/java/com/oligei/ticket_gathering/daoimpl/ActivityDaoImpl.java
@@ -1,13 +1,17 @@
 package com.oligei.ticket_gathering.daoimpl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.oligei.ticket_gathering.dao.ActitemDao;
 import com.oligei.ticket_gathering.dao.ActivityDao;
 import com.oligei.ticket_gathering.entity.mongodb.ActivityMongoDB;
+import com.oligei.ticket_gathering.entity.mysql.Actitem;
 import com.oligei.ticket_gathering.entity.mysql.Activity;
 import com.oligei.ticket_gathering.repository.ActivityMongoDBRepository;
 import com.oligei.ticket_gathering.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -19,13 +23,26 @@ public class ActivityDaoImpl implements ActivityDao {
     @Autowired
     private ActivityMongoDBRepository activityMongoDBRepository;
 
+
     @Override
     public Activity findOneById(Integer id) {
-        Activity activity = activityRepository.getOne(id);
-        Optional<ActivityMongoDB> activityMongoDB = activityMongoDBRepository.findById(id);
-        if(activityMongoDB.isPresent()){
-            activity.setDescription(activityMongoDB.get().getDescription());
+        Activity activity;
+        try {
+            activity = activityRepository.getOne(id);
+            Optional<ActivityMongoDB> activityMongoDB = activityMongoDBRepository.findById(id);
+            if (activityMongoDB.isPresent()) {
+                activity.setDescription(activityMongoDB.get().getDescription());
+            }
+            return activity;
+        } catch (javax.persistence.EntityNotFoundException e) {
+            System.out.println("Wrong activityId!");
+            return null;
         }
-        return activity;
     }
+
+    @Override
+    public List<Activity> findAllByTitleOrVenueOrActor(String title, String venue, String actor) {
+        return activityRepository.findAllByTitleLikeOrVenueLikeOrActorLike(title, venue, actor);
+    }
+
 }
---
+++ backend/src/test/java/com/oligei/ticket_gathering/service/ActivityServiceTest.java
@@ -0,0 +1,45 @@
+package com.oligei.ticket_gathering.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class ActivityServiceTest {
+
+    @Autowired
+    private ActivityService activityService;
+
+    @Test
+    @Transactional
+    @Rollback
+    void findActivityAndActitem(){
+        System.out.println("Correct ActivityId");
+        assertEquals(activityService.findActivityAndActitem(1).getInteger("activityId"),1);
+        assertEquals(activityService.findActivityAndActitem(100).getInteger("activityId"),100);
+        assertEquals(activityService.findActivityAndActitem(1000).getInteger("activityId"),1000);
+        System.out.println("Wrong ActivityId");
+        assertNull(activityService.findActivityAndActitem(1500));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void search(){
+        System.out.println("Null Value");
+        assertNull(activityService.search(null));
+        System.out.println("Preparation");
+        activityService.search("周杰伦演唱会");
+        System.out.println("Reasonable Value");
+        assertTrue(activityService.search("周杰伦").size()>0);
+        assertTrue(activityService.search("周杰伦演唱会").size()>0);
+        assertTrue(activityService.search("周杰伦演唱会螺蛳粉肉丸子").size()>0);
+        System.out.println("Unreasonable Value");
+        assertEquals(0, activityService.search("12345").size());
+        assertEquals(0, activityService.search("螺蛳粉肉丸子").size());
+    }
+
+}
