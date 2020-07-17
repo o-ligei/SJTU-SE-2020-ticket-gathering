39a32a34274396da664a6a74b14ecfe44b62b7f8
feaaaaaa <409834725@qq.com>
2020-07-18 00:15:47 +0800

    fix: merge1.0

--- backend/src/test/java/com/oligei/ticket_gathering/dao/OrderDaoTest.java
+++ backend/src/test/java/com/oligei/ticket_gathering/dao/OrderDaoTest.java
@@ -7,6 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,5 +36,12 @@ class OrderDaoTest {
         System.out.println("using none-existed userId to test");
         test=orderDao.getOrderByUserId(noneExistedUserId);
         assertEquals(0,test.size());
+    }
+
+    @Test
+    @Rollback
+    void addOrder() throws ParseException {
+        DateFormat format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        assertTrue(orderDao.addOrder(1,8,80,1,format2.parse("2020-02-21 16:00:00"),format2.parse("2020-02-21 16:00:00")));
     }
 }
