99b6b01a60cc3fffe0788ec977c16aa07ec3d396
feaaaaaa <409834725@qq.com>
2020-07-24 13:59:07 +0800

    fix: mysql for auction

---
+++ Sample_Database/auctions.sql
@@ -0,0 +1,17 @@
+DROP TABLE if EXISTS tg_auctions;
+
+create table tg_auctions
+(
+    auctionid INT NOT NULL AUTO_INCREMENT,
+    actitemid INT,
+    userid INT,
+    isover BIT DEFAULT 0,
+    price INT,
+    ddl DATETIME,
+    showtime DATE,
+    ordertime DATETIME,
+    amount INT,
+
+    PRIMARY KEY(auctionid),
+    FOREIGN KEY(actitemid) REFERENCES tg_actitems(actitemid)
+);
